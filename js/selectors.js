adcfa9cf0ae44f5e2a4daa203879fd880c60fd9b
Melany Yeung <mwyeung21@gmail.com>
2018-06-22 13:59:35 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,2 +1,25 @@
 'use-strict';
 //add your code here
+
+function h1Selector(){
+  return $('h1');
+}
+
+function liInOlSelector(){
+  return $('ol li');
+  //return $("ol").children()
+}
+
+function linkSelector(){
+  return $('.box5#box4 a');
+  //order doesnt matter '#box4.box5 a' also works
+  // return $("#box4.box5").children()
+}
+
+function imageSelector(){
+  return $('img[alt="cat sleeping"]');
+}
+
+function checkboxInputSelector(){
+  return $('input[type="checkbox"]');
+}
